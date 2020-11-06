75014b378d4fdfb4409bddbde526beb633226eef
wayne <jun_wei_like1@163.com>
2020-11-06 11:51:31 +0800

    propTypes与defaultProps

--- src/todolist/TodoItem.js
+++ src/todolist/TodoItem.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+/**
+ * propTypes 和 defaultProps
+ * 官方文档：https://reactjs.org/docs/typechecking-with-proptypes.html
+ *
+ *
+ * */
 
-const TodoItem = ({content, onItemClick}) => {
+const TodoItem = ({content, test, onItemClick}) => {
     return (
         <div onClick={onItemClick}>
-            {content}
+            {test}:{content}
         </div>
     )
+}
+// 属性校验
+TodoItem.propTypes = {
+    content: PropTypes.arrayOf(PropTypes.string, PropTypes.number),
+    onItemClick : PropTypes.func,
+}
+// 设置属性的默认值
+TodoItem.defaultProps = {
+    test: '测试'
 }
 
 export default TodoItem;
