505d66742c2fd0a5355371a56f751154beb9710b
wayne214 <jun_wei_like1@163.com>
2020-11-20 18:07:56 +0800

    transition group

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, {Component, Fragment} from 'react'
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './app.css'
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            show: true,
+            list: []
         }
     }
 
     _handleTraggle = () => {
-        this.setState(()=>({
-            show: !this.state.show
-        }))
+        // this.setState(()=>({
+        //     show: !this.state.show
+        // }))
+
+        this.setState((prevState)=>{
+            return {
+                list: [...prevState.list, 'item']
+            }
+        })
     }
 
     render() {
@@ -29,6 +36,23 @@ class App extends Component {
                 >
                     <div>Hello react-transition</div>
                 </CSSTransition>
+
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                <CSSTransition
+                                    timeout={2000}
+                                    classNames='fade'
+                                    unmountOnExit
+                                    key={item+index+''}
+                                >
+                                    <div>Hello transition item {index}</div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
                 <button onClick={this._handleTraggle}>traggle</button>
             </Fragment>
 
