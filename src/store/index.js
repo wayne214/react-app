90714bda9a193070dba358aa7695e3b9e33a82c3
wayne214 <jun_wei_like1@163.com>
2020-11-22 10:40:43 +0800

    redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,22 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from "redux-thunk";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+
+const store = createStore(
+    reducer,
+    enhancer,
+    );
 
 export default store;
